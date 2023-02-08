cb5cd9dcf67a5c12dce83066fa202d8947a271e7
Lex Mullin <alemulli@gmail.com>
2023-02-08 16:15:32 -0500

    added negative functionality to the - button, and added some error handling regarding the - button and the . button to avoid numbers with multiple decimals and to avoid multiple - symbols at the beginning of a number

--- calc.ts
+++ calc.ts
@@ -39,11 +39,19 @@ function concatNumToDisplay(appendedNumber: string) {
 }
 
 button1.addEventListener("click", function () {
-  concatNumToDisplay(button1.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button1.value);
+  }
 });
 
 button2.addEventListener("click", function () {
-  concatNumToDisplay(button2.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button2.value);
+  }
 });
 
 button3.addEventListener("click", function () {
@@ -51,106 +59,171 @@ button3.addEventListener("click", function () {
 });
 
 button4.addEventListener("click", function () {
-  concatNumToDisplay(button4.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button4.value);
+  }
 });
 
 button5.addEventListener("click", function () {
-  concatNumToDisplay(button5.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button5.value);
+  }
 });
 
 button6.addEventListener("click", function () {
-  concatNumToDisplay(button6.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button6.value);
+  }
 });
 
 button7.addEventListener("click", function () {
-  concatNumToDisplay(button7.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button7.value);
+  }
 });
 
 button8.addEventListener("click", function () {
-  concatNumToDisplay(button8.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button8.value);
+  }
 });
 
 button9.addEventListener("click", function () {
-  concatNumToDisplay(button9.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button9.value);
+  }
 });
 
 button0.addEventListener("click", function () {
-  concatNumToDisplay(button0.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    concatNumToDisplay(button0.value);
+  }
 });
 
+
+// decimal button display 0. if decimal is hit before any other number to avoid resulting in errors
+// if the decimal button is hit twice in a row resulting in "0.." it throws an error
+// if a decimal already exists within the displays string it throws an error
+
 buttonDecimal.addEventListener("click", function () {
-  concatNumToDisplay(buttonDecimal.value);
+  if (display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else if (display.innerText.includes(".")) {
+    display.innerText = "ERROR"
+  } else {
+    if (display.innerText === "0") {
+      concatNumToDisplay("0.");
+    } else {
+      concatNumToDisplay(buttonDecimal.value);
+    }
+  }
 });
 
 //////////////////// Clear Display ////////////////////
 
-function clear () {
-    display.innerText = "0"
+function clear() {
+  display.innerText = "0";
 }
 
 buttonClear.addEventListener("click", function () {
-    clear();
-  });
+  clear();
+});
 
 //////////////////// Clear All ////////////////////
 
-function clearAll () {
-    display.innerText = "0"
-    console.log (value1, value2, operation)
-    value1 = undefined
-    value2 = undefined
-    operation = undefined
-    console.log (value1, value2, operation)
+function clearAll() {
+  display.innerText = "0";
+  console.log(value1, value2, operation);
+  value1 = undefined;
+  value2 = undefined;
+  operation = undefined;
+  console.log(value1, value2, operation);
 }
 
 buttonClearAll.addEventListener("click", function () {
-    clearAll();
+  clearAll();
 });
 
 //////////////////// Operations ////////////////////
 
-function setOperation (clickedOperation: string) {
-    operation = clickedOperation
+function setOperation(clickedOperation: string) {
+  operation = clickedOperation;
 }
 
-// storeValue1 also updates value1 if the user is performing a second operation as opposed to hitting the equals button after inputing a second value. 
-
-function storeValue1 () {
-    if (value1 === 0 || value1 === undefined) {
-        value1 = +display.innerText
-    } else if (operation === "add"){
-        value1 += +display.innerText
-    } else if (operation === "subtract") {
-        value1 -= +display.innerText
-    } else if (operation === "multiply") {
-        value1 = value1 * +display.innerText
-    } else {
-        value1 = value1 / +display.innerText
-    }
+// storeValue1 also updates value1 if the user is performing a second operation as opposed to hitting the equals button after inputing a second value.
+// this function also checks to see if they have tried to perform an operation on "-" with no number, if so it will return error
+function storeValue1() {
+  if (value1 === 0 || value1 === undefined) {
+    value1 = +display.innerText;
+  } else if (operation === "add") {
+    value1 += +display.innerText;
+  } else if (operation === "subtract") {
+    value1 -= +display.innerText;
+  } else if (operation === "multiply") {
+    value1 = value1 * +display.innerText;
+  } else {
+    value1 = value1 / +display.innerText;
+  }
 }
 
-buttonAdd.addEventListener ("click", function () {
+buttonAdd.addEventListener("click", function () {
+  if (display.innerText === "-" || display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
     setOperation(buttonAdd.value);
     storeValue1();
     clear();
-})
+  }
+});
 
-buttonSubtract.addEventListener ("click", function () {
-    setOperation(buttonSubtract.value);
-    storeValue1();
-    clear();
-})
+// subtract button puts a negative symbol in the display to make a number negative if the display is currently 0
 
-buttonMultiply.addEventListener ("click", function () {
+buttonSubtract.addEventListener("click", function () {
+  if (display.innerText === "-" || display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
+    if (display.innerText === "0") {
+      concatNumToDisplay("-");
+    } else {
+      setOperation(buttonSubtract.value);
+      storeValue1();
+      clear();
+    }
+  }
+});
+
+buttonMultiply.addEventListener("click", function () {
+  if (display.innerText === "-" || display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
     setOperation(buttonMultiply.value);
     storeValue1();
     clear();
-})
+  }
+});
 
-buttonDivide.addEventListener ("click", function () {
+buttonDivide.addEventListener("click", function () {
+  if (display.innerText === "-" || display.innerText === "ERROR") {
+    display.innerText = "ERROR";
+  } else {
     setOperation(buttonDivide.value);
     storeValue1();
     clear();
-})
+  }
+});
 
-//what's left - calculate and return the result upon hitting the equals button (ifs for if there is only a value 1 input)(clear value1 value2 and operator, but user can still use an operator from here to perform an opertation on the result of the previous calculation), make the subtract button make a negative symbol if there is no numbers input into the display yet, css styling
+//what's left - calculate and return the result upon hitting the equals button (ifs for if there is only a value 1 input)(clear value1 value2 and operator, but user can still use an operator from here to perform an opertation on the result of the previous calculation),  css styling
